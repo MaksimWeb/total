4c8af55caa1aec028f3025a31bcdc2d314eff3e6
Maxim Yugay <sayamonkoh@gmail.com>
2023-11-30 23:55:31 +0300

    webpack config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from './components/counter';
+import './index.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      asdas
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFn(arg: number): string {
+  console.log('Someee fn 222');
+
+  return '';
+}
